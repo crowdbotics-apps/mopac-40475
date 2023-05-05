df1b68125e66bb616e54fa282c639c82d9ca755d
Crowdbotics <team@crowdbotics.com>
2023-05-05 10:47:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mopac-40475",
+  url: "https://mopac-40475.botics.co",
+  api: "https://mopac-40475.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
